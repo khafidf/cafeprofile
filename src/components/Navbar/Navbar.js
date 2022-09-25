6e64b109843817396577673e6ed2461852301be6
khafidf <104146117+khafidf@users.noreply.github.com>
2022-09-26 02:44:12 +0700

    up

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import { React, useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { IconContext } from "react-icons";
+import './Navbar.css'
+
+const Navbar = () => {
+
+  const [icon, setIcon] = useState(false);
+
+  const activeDropdown = () => {
+    const navBar = document.querySelector('.nav-list');
+    navBar.classList.toggle('active');
+    setIcon(!icon);
+  }
+
+  return (
+    <div className='nav-container'>
+        <div className='nav-logo'>
+            <h1>Coffe<span>Co</span></h1>
+        </div>
+        <div className='hamburger' onClick={() => activeDropdown()}>
+          <IconContext.Provider value={{ className: 'line' }}>{icon ? <FaTimes /> : <FaBars />}</IconContext.Provider>
+        </div>
+        <div className='nav-list'>
+            <ul>
+                <li><a href=''>Profil</a></li>
+                <li><a href=''>Produk</a></li>
+                <li><a href=''>Tempat dan Suasana</a></li>
+                <li><a href=''>Peta</a></li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Product.css'
+
+const Product = () => {
+  return (
+    <div className='product-container'>
+        <div className='product-'> </div>
+    </div>
+  )
+}
+
+export default Product
